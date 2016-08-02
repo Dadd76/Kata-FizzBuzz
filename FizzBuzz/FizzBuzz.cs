4e7e3ffa0ab16d927f7fdc3c3019076e90ece520
Yannick <yannick.darcillon@gmail.com>
2016-08-03 00:46:50 +0200

    first commit:

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fizz
+{
+    public static class FizzBuzz
+    {
+
+
+        public static string GetMessage(this int value)
+        {
+            string result = string.Empty;
+
+            if (value.IsModuloOf(3))
+            {
+                result += "Fizz";
+            }
+
+            if (value.IsModuloOf(5))
+            {
+                result += "Buzz";
+            }
+
+            return ( string.IsNullOrEmpty(result) ? value.ToString() : result);
+        }
+
+        //private bool IsModuloOf(int value, int modulo)
+        //{
+        //    return (value % modulo == 0);
+        //}
+
+
+        private static bool IsModuloOf(this int value, int modulo)
+        {
+            return (value % modulo == 0);
+        }
+
+    }
+}
---
+++ FizzBuzz/FizzBuzzTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+
+namespace Fizz
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(4)]
+        public void TestThatCommonNumberReturnNumber(int i)
+        {
+            
+            string message = string.Empty;
+
+            FizzBuzz.GetMessage(i);
+            Assert.AreEqual(message, i.ToString());
+
+        }
+
+        [TestCase(3)]
+        [TestCase(9)]
+        public void TestThatNumberMutipleOf3ReturnFizz(int i)
+        {
+            string message = string.Empty;       
+            message = FizzBuzz.GetMessage(i);
+            Assert.AreEqual(message,"Fizz");
+        }
+
+        [TestCase(5)]
+        [TestCase(10)]
+        public void TestThatNumberMutipleOf5ReturnFizz(int i)
+        {
+            string message = string.Empty;
+            message = FizzBuzz.GetMessage(i);
+            Assert.AreEqual(message, "Buzz");
+        }
+
+        [TestCase(15)]
+        [TestCase(30)]
+        public void TestThatNumberMutipleOf3and5ReturnFizz(int i)
+        {
+            string message = string.Empty;
+
+            message = FizzBuzz.GetMessage(i);
+            Assert.AreEqual(message, "FizzBuzz");
+        }
+
+    }
+}
